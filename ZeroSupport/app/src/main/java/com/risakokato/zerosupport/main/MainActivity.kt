db5700b63ff672e14f1efd3c8c22a79e4b048af1
SoutaHagiwara <0104.seibu6000@gmail.com>
2019-03-10 17:18:47 +0900

    activity_mainを整理

--- ZeroSupport/app/src/main/java/com/risakokato/zerosupport/main/MainActivity.kt
+++ ZeroSupport/app/src/main/java/com/risakokato/zerosupport/main/MainActivity.kt
@@ -1,14 +1,14 @@
-package com.risakokato.zerosupport.main
-
-import android.app.Activity
-import android.os.Bundle
-import com.risakokato.zerosupport.R
-
-class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-    }
-}
+//package com.risakokato.zerosupport.main
+//
+//import android.app.Activity
+//import android.os.Bundle
+//import com.risakokato.zerosupport.R
+//
+//class MainActivity : Activity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//
+//    }
+//}
