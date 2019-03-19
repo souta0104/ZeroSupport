488af57f365bd9c5e1020218f22a39fa266f3f7b
SoutaHagiwara <0104.seibu6000@gmail.com>
2019-03-19 17:16:26 +0900

    deleteAllメソッドを追加

--- ZeroSupport/app/src/main/java/com/risakokato/zerosupport/model/BelongingsRepository.kt
+++ ZeroSupport/app/src/main/java/com/risakokato/zerosupport/model/BelongingsRepository.kt
@@ -1,24 +1,23 @@
 package com.risakokato.zerosupport.model
 
-import com.risakokato.zerosupport.ext.getToday
 import com.risakokato.zerosupport.model.dao.BelongingsDao
 import com.risakokato.zerosupport.model.entity.BelongingsRoom
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.*
 
 class BelongingsRepository(val db: BelongingsDao, private val scope: CoroutineScope) {
     init {
-        scope.launch {
-            db.add(BelongingsRoom(0, "tsdata", Date().getToday(), Date().getToday(), false))
-        }
+
     }
 
     suspend fun getAll(): List<BelongingsRoom> {
         return withContext(scope.coroutineContext) {
             db.findAll()
         }
+    }
+
+    fun deltetAll() {
+        db.deleteAll()
     }
 
 }
